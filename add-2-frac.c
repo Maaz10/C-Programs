d3f9c6f12d7f6b96bf44c1d07858d50781b7c5ad
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-11 16:39:11 +0530

    Add files via upload

---
+++ add-2-frac.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+ struct frac
+{
+ float x,y;                      
+};
+int input(struct frac* h)
+{
+ printf("Enter numerator of 1st");
+ scanf("%f",&h[0].x);
+
+ printf("Enter denomenator of 1st");
+ scanf("%f",&h[0].y);
+
+ printf("Enter numerator of 2nd");
+ scanf("%f",&h[1].x);
+
+ printf("Enter denomenator of 2nd");
+ scanf("%f",&h[1].y);
+
+ return 0;
+}
+int compute(struct frac* h)
+{
+ float k=0,sum=0;
+     k=(h[0].y*h[1].y);
+    sum=((h[0].x*h[1].y)+(h[0].y*h[1].x))/k;
+ return sum;
+}
+void output(float o)
+{
+ printf("the sum is %f ",o);
+}
+int main()
+{
+ struct frac h[2];
+ float c;
+ input(h);
+ c=compute(h);
+ output(c);
+ return 0;
+}
+    
+    
