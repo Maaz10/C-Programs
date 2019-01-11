b7a6a4ba42934276c90297529f9623e510ee461e
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-11 11:30:52 +0530

    Update add-n.c

--- add-n.c
+++ add-n.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
-int input(int n, int m[100])
+int input(int *c, int m[])
 {
+ int i;
  printf("Enter the number of inputs ");
- scanf("%d",&n);
+ scanf("%d",c);
  printf("Enter the numbers ");
- for(int i=0;i<n;i++)
-   scanf("%d",&m[100]);
+ for( i=0;i<*c;i++)
+   scanf("%d",&m[i]);
+ return 0;
 }
-int add(int n[100])
+int add(int l, int n[])
 {
- int x;
- for(int i=0;i<n[100];i++)
+ int x=0,i;
+ for( i=0;i<l;i++)
    x+=n[i];
  return x;
 }
@@ -20,8 +22,10 @@ void output(int z)
 }
 int main()
 {
- int n[100],l,z;
- input(l,n[100]);
- z=add(n[100]);
+ int n[100];
+ int l,z;
+ input(&l,n);
+ z=add(l,n);
  output(z);
+ return 0;
 }
