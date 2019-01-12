69f18e9bfd9159570f08772ce312ae7ef89ca920
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-12 10:25:03 +0530

    Update add-2-frac-PS.c

--- add-2-frac-PS.c
+++ add-2-frac-PS.c
@@ -5,31 +5,31 @@ int x,y;
 }f;
 int input(f* h1,f* h2)
 {
- printf("Enter numerator of 1st");
- scanf("%f",&h1->x);
+ printf("Enter numerator of 1st ");
+ scanf("%d",&h1->x);
 
- printf("Enter denomenator of 1st");
- scanf("%f",&h1->y);
+ printf("Enter denomenator of 1st ");
+ scanf("%d",&h1->y);
 
- printf("Enter numerator of 2nd");
- scanf("%f",&h2->x);
+ printf("Enter numerator of 2nd ");
+ scanf("%d",&h2->x);
 
- printf("Enter denomenator of 2nd");
- scanf("%f",&h2->y);
+ printf("Enter denomenator of 2nd ");
+ scanf("%d",&h2->y);
 
  return 0;
 }
-int compute(f* h1,f* h2)
+double compute(f* h1,f* h2)
 {
- int k=0;
- float sum=0;
-     k=( h1->y * h2->y );
+ double k=0;
+ double sum=0;
+    k=( h1->y * h2->y );
     sum=(( h1->x * h2->y )+( h1->y * h2->x ))/k;
  return sum;
 }
 void output(float o)
 {
- printf("the sum is %f ",o);
+ printf("the sum is %f \n",o);
 }
 int main()
 {
@@ -39,4 +39,5 @@ int main()
  c=compute(&h1,&h2);
  output(c);
 }
+   
     
