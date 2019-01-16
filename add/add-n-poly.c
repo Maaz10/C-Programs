b26f0a630e5da387f0a0498b95254ff9486d11fa
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-16 16:11:47 +0530

    Update add-n-poly.c

--- add/add-n-poly.c
+++ add/add-n-poly.c
@@ -1,28 +1,32 @@
 #include<stdio.h>
+#include<math.h>
 
 int getn(int* n )
 {
+ return 0;
 }
  
-int input(int n ,int coef[], int x[], int p[])
+int input(int n ,int coef[n], int x[n])
 {
  int i;
  printf("Enter the number of terms in the polynomial ");
  scanf("%d",&n);
  printf("Enter the terms");
  for(i=0;i<n;i++)
-  scanf("%d*(%d)^%d",coef,x,p);
+  scanf("%d*%d",&coef[i],&x[i]);
  return 0;
 }
 
-int add(int n,int coef[],int x[],int p[])
+int add(int n,int coef[],int x[], int z[],int* sum )
 {
- int i;
- for(i=0;i<p;i++)
-   x *= x[i];
- for(i=0;i)
-    
-
+ int i,m;
+ m=n-1;
+ for(i=0;i<m;i++)
+  {
+   z[i] *=(coef[i])*(pow(x[i], i));
+   *sum += z[i]; 
+  }
+ return 0;
 }
 
 void output(int x)
@@ -33,9 +37,9 @@ int main()
 {
  int n;
  getn(&n);
- int x[n],y[n],z[n];
- input(n,x,y,z);
- add();
- output(x,y,z);
+ int x[n],y[n],c[n],s=0;
+ input(n,x,y);
+ add(n,x,y,c,&s);
+ output(s);
  return 0;
 }
