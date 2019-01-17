6b1ca53868fa742615e1860f275b10398aeb0d97
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-17 12:26:44 +0530

    Create pyramid.c

---
+++ lab/pyramid.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int input(int* n)
+{
+ printf("Enter the number of rows ");
+ scanf("%d",n);
+ return 0;
+}
+void output(int n ,int* x)
+{
+ int i,j;
+ for( i=0;i<n;i++)
+ {
+   printf("\n \t");
+   
+       for(j=i;j<(2*i)+1;j++)
+        {
+         printf(" *");
+        }
+        
+      
+  }
+ printf("\n");
+}
+int main()
+{  
+ int n;
+ input(&n);
+ int x[n];
+ output(n,x);
+ return 0;
+}
+ 
