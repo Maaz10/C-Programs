3a4d23e7c85ac6b43cc57f7f7ae5e90be5b2589f
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-17 18:43:55 +0530

    using  structures

--- lab/quad.c
+++ lab/quad.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef struct complex
+{ 
+ double x;
+ double y;
+}comp;
+
 int input(int* a, int* b, int* c )
 {
  printf("Enter th coeffecients of a b and c ");
  scanf("%d %d %d",a,b,c);
-}
+ return 0;
+ }
 
-int compute(int a, int b, int c, int* y, int* z)
+int compute(int a, int b, int d,comp* c)
 {
- int x;
- x=(b*b)-(4*a*c);
- *y=((-b)+(sqrt(x)))/(2*a);
- *z=((-b)-(sqrt(x)))/(2*a);
+ int x,y,z,f;
+ 
+ z=(2*a);
+ f=-b;
+ x=(b*b)-(4*a*d);
+ y=(pow(x,0.5));
+ c->x=((f+y)/z);
+ c->y=((f-y)/z);
+  printf("the roots are %f and %f ",c->x,c->y);
  return 0;
 }
-void output(int x, int y)
+void output(comp c)
 {
- printf("the roots are %d and %d ",x,y);
+ printf("the roots are %f and %f ",c.x,c.y);
 }
 int main(void)
 {
- int a,b,c,y,z;
- input(&a,&b,&c);
- compute(a,b,c,&y,&z);
- output(y,z);
+ comp c;
+ int a,b,d;
+ input(&a,&b,&d);
+ compute(a,b,d,&c);
+ output(c);
  return 0;
 }
