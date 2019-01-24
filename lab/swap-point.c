0f610cb5f726dfcfb1bda825b5ca735752636b01
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-24 17:15:29 +0530

    Create swap-point.c

---
+++ lab/swap-point.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int input(int* x,int* y)
+{
+ printf("Enter two numbers ");
+ scanf("%d %d",x,y);
+ return 0;
+}
+
+int compute(int* x,int* y,int* t)
+{
+ *t=*x;
+ *x=*y;
+ *y=*t;
+ return 0;
+}
+ 
+void output(int x, int y)
+{
+ printf("After swaping the numbers are %d and %d",x,y);
+}
+
+int main(void)
+{
+ int x,y,z;
+ input(&x,&y);
+ compute(&x,&y,&z);
+ output(x,y);
+ return 0;
+}
