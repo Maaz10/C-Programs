ff64b9841ea8803f2f2e76661115b5f01115c7f9
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-24 11:35:30 +0530

    Create grade.c

---
+++ lab/grade.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+
+int input()
+{
+   int z;
+   printf("Enter the number ");
+   scanf("%d",&z);
+   return z;
+}
+
+int compute(int x)
+{
+
+  if(x>=95)
+  {
+   printf("A+");
+  }
+  else
+   if(x>=90)
+   {
+    printf("A");
+   }
+  else
+   if(x>=85)
+   {
+    printf("B+");
+   } 
+  else 
+   if(x>=80)
+   { 
+   printf("B");
+   }
+  else 
+   if(x>=75)
+   {
+    printf("C+");
+   }
+  else 
+   if(x>=70)
+   {
+    printf("C");
+   }
+  else 
+   if(x>=60)
+   {
+    printf("D+");
+   }
+  else 
+   if(x>=50)
+   {
+    printf("D");
+   }
+  else 
+   if(x>=40)
+   {
+    printf("E");
+   }
+  else
+  {
+   printf("FAIL");
+  }
+ return 0;
+}
+int main(void)
+{
+ int n=input();
+ compute(n);
+ return 0;
+}
+
+
+
+
+
+ 
