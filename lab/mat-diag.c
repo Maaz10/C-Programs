c6efc54e62833ca3a99b4fee9cc89ef7ac3734ac
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-28 17:58:41 +0530

    Update mat-diag.c

--- lab/mat-diag.c
+++ lab/mat-diag.c
@@ -1,40 +1,55 @@
 #include<stdio.h>
 
-int getn(int* m,int* n)
+void getn(int* m)
 {
- printf("Enter the number of rows ");
+ printf("Enter the number of rows/columns ");
  scanf("%d",m);
- printf("Enter the number of columns ");
- scanf("%d",n);
- return 0;
 }
 
-int input(int m,int n,int x[][])
+void input(int m,int x[m][m])
 {
  printf("Enter the elements \n");
  for(int i=0;i<m;i++)
  {
-  for(int j=0;j<n;j++)
+  for(int j=0;j<m;j++)
    {
     scanf("%d",&x[i][j]);
    }
  }
- return 0;
 }
 
-int compute()
+void compute(int m,int x[m][m],int y[m] )
 {
- int n=100;
   for(int i=0;i<m;i++)
  {
-  for(int j=0;j<n;j++)
+  for(int j=0;j<m;j++)
    {
     if(i == j)
      {
-      for(int k=0;k<n;i++)
-      
+       for(int k=i;k<m;k++)    
+        y[k]=x[i][j];
+     }
+   }
+  } 
+}
 
+void output(int m,int y[m])
+{
+ printf("The diagonal elements are : \n");
+ for(int i=0;i<m;i++)
+ printf("%d\n",y[i]);
+}
 
+int main()
+{
+ int m;
+ getn(&m);
+ int x[m][m],y[m];
+ input(m,x);
+ compute(m,x,y);
+ output(m,y);
+ return 0;
+}
 
 
 
