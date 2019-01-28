2a190c9076775f92346340effc2ded82543bafb9
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-01-28 18:56:05 +0530

    logical error

---
+++ lab/quad-cmplx.c
@@ -0,0 +1,60 @@
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct complex
+{ 
+ long long x;
+ long long y;
+}comp;
+
+int input(int* a, int* b, int* c )
+{
+ printf("Enter th coeffecients of a b and c ");
+ scanf("%d %d %d",a,b,c);
+ return 0;
+ }
+
+int compute(int a, int b, int d,comp* c1,int* x)
+{
+ int c,y,z,f;
+ 
+ z=(2*a);
+ f=-b;
+ *x=(b*b)-(4*a*d);
+ if(x<0)
+ {
+  c=-(*x);
+  c1->x=f/z;
+  c1->y=(sqrt(c))/z;
+ }
+ else
+ {
+  y=(pow(*x,0.5));
+  c1->x=((f+y)/z);
+  c1->y=((f-y)/z);
+ }
+ return 0;
+}
+void output(comp c1,int z)
+{
+ if(z<0)
+ {
+ printf("the roots are %lld and %lld ",c1.x,c1.y);
+ }
+ else
+ {
+ printf("roots are imaginary \n");
+ printf("%lld+i(%lld) and %lld-i(%lld)",c1.x,c1.y,c1.x,c1.y);
+ }
+}
+int main(void)
+{
+ comp c1;
+ int a,b,d;
+ int x;
+ input(&a,&b,&d);
+ compute(a,b,d,&c1,&x);
+ output(c1,x);
+ return 0;
+}
