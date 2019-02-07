46af0f35e8965ac2d327190ffdc873f7912d65ff
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-02-07 12:44:46 +0530

    Update per-conb.c

--- lab/per-conb.c
+++ lab/per-conb.c
@@ -8,9 +8,9 @@ void input(int* n,int*r)
  printf("Enter the value of r:");
  scanf("%d",r);
 }
-void compute(int n,int r,int* p)
+void compute(int n,int r,int* p,int* c)
 {
- int a=1,b=1,f,d;
+ int a=1,b=1,e=1,f,d;
  f=n;
  d=n;
  for(int i=0;i<n;i++)
@@ -37,21 +37,34 @@ void compute(int n,int r,int* p)
      f--;
     }
  }
+ for(int i=0;i<n;i++)
+  {
+   if(r<=0)
+    {
+     break;
+    }
+    else
+    {
+     e=e*r;
+     r--;
+    }
+ }
    *p=a/b;
+   *c=a/(b*e);
 }
 
-void output(int p)
+void output(int p,int c)
 {
  printf("The Permeutation is %d",p);
- 
+ printf("The Combination is %d",c);
  
 }
 
 int main()
 {
- int n,r,p;
+ int n,r,p,c;
  input(&n,&r);
- compute(n,r,&p);
- output(p);
+ compute(n,r,&p,&c);
+ output(p,c);
  return 0;
 }
