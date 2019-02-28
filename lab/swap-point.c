967f5d64847844cd0167a66953cd123f614361c3
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-02-28 11:22:14 +0530

    Update swap-point.c

--- lab/swap-point.c
+++ lab/swap-point.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 
-int input(int* x,int* y)
+void input(int* x,int* y)
 {
  printf("Enter two numbers ");
  scanf("%d %d",x,y);
- return 0;
 }
 
-int swap(int* x,int* y)
+void swap(int* x,int* y)
 {
  int t;
  t=*x;
  *x=*y;
  *y=t;
- return 0;
 }
  
 void output(int x, int y)
