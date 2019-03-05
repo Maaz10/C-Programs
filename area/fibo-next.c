f35872e244f44429e5d821e1d3e3a0ad4a577b84
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-05 18:28:33 +0530

    Create fibo-next.c

---
+++ area/fibo-next.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void next(int i);
+
+void input(int* n)
+{
+	printf("Enter the number of terms needed ");
+	scanf("%d",n);
+}
+
+int nexti(int i)
+{
+ int x[100];
+ x[0]=1;
+ x[1]=1;
+	  x[i] = x[i-1] + x[i-2]; 
+ return x[i];
+}
+
+void output(int n)
+{
+	printf("The Fibonacci seires is \n 1 1");
+	for(int i=2;i<n;i++)
+	 {
+	  printf(" %d",nexti(i));
+	 }
+}
+
+int main()
+{
+	int n;
+	input(&n);
+	output(n);
+	return 0;
+}
+	   
