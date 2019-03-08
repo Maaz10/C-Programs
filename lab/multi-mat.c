5b967bfe56353fe0693450cb6e22ce5917159356
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-08 12:44:26 +0530

    Update multi-mat.c

--- lab/multi-mat.c
+++ lab/multi-mat.c
@@ -1,47 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void getn(int* m1,int* m2,int* n1,int* n2,int* m,int* n)
+void getn(int* m1,int* m2,int* n1,int* n2)
 {
  printf("Enter the no of rows and columns for 1st matrix: ");
  scanf("%d %d",m1,n1);
  printf("Enter the no of rows and columns for 2nd matrix: ");
  scanf("%d %d",m2,n2);
- if(*m1<*m2)
- {
-  *m=*m1;
- }
- else
-  *m=*m2;
- if(*n1<*n2)
- {
-  *n=*n1;
- }
- else
-  *n=*n2;
 }
 
-void input(int m1,int n1,int m2,int n2,int x[m1][n1],int y[m2][n2])
+void input(int m,int n,int x[m][n])
 {
  printf("Enter the elements of 1st matrix\n");
- for(int i=0;i<m1;i++)
+ for(int i=0;i<m;i++)
  {
-  for(int j=0;j<n1;j++)
+  for(int j=0;j<n;j++)
   {
    scanf("%d",&x[i][j]);
    }
   }
- printf("Enter the elements of 2nd matrix\n");
- for(int k=0;k<m2;k++)
- {
-  for(int l=0;l<n2;l++)
-  {
-   scanf("%d",&y[k][l]);
-   }
-  }
 }
 
-void multi(int m1,int n1,int m2,int n2,int x[m1][n1],int y[m2][n2],int z[100][100])
+int multi(int m1,int n1,int m2,int n2,int x[m1][n1],int y[m2][n2],int z[m1][n2])
 {
  if(n1!=m2)
  {
@@ -62,15 +42,16 @@ void multi(int m1,int n1,int m2,int n2,int x[m1][n1],int y[m2][n2],int z[100][10
    }
   }
  }
+ return 0;
 }
 
-void output(int m,int n,int z[100][100])
+void output(int m1,int n2,int z[m1][n2])
 {
  printf("The elements of the matrix after multiplication are\n");
- for(int i=0;i<m;i++)
+ for(int i=0;i<m1;i++)
  {
   printf("\n");
-  for(int j=0;j<n;j++)    
+  for(int j=0;j<n2;j++)    
   {
    printf(" %d",z[i][j]);
   }
@@ -79,13 +60,12 @@ void output(int m,int n,int z[100][100])
 
 int main()
 {
- int m1,m2,n1,n2,m,n;
- getn(&m1,&m2,&n1,&n2,&m,&n);
- int x[m1][n1],y[m2][n2],z[100][100];
- input(m1,n1,m2,n2,x,y);
+ int m1,m2,n1,n2;
+ getn(&m1,&m2,&n1,&n2);
+ int x[m1][n1],y[m2][n2],z[m1][n2];
+ input(m1,n1,x);
+ input(m2,n2,y);
  multi(m1,n1,m2,n2,x,y,z);
- output(m,n,z);
+ output(m1,n2,z);
  return 0;
 }
-
-
