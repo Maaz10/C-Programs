90ec43c828e3c3a9a27b2a41af653d3a03f7b301
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-08 15:57:50 +0530

    Update armstrong.c

--- lab/armstrong.c
+++ lab/armstrong.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void input(int* n)
+void inputnumber(int* n)
 {
  printf("Enter the number ");
   scanf("%d",n);
@@ -9,7 +9,7 @@ void input(int* n)
 void armstrong(int n,int* a,int* b)
 { 
  int y=0;
-  *b=x;   
+  *b=n;   
  while (n != 0)
     {
         y = n % 10;
@@ -17,22 +17,22 @@ void armstrong(int n,int* a,int* b)
         n=n/10;
     }
 }
-void output(int a,int b)
+void outputresult(int a,int b)
 {
  printf("the sum is %d",a);
   if(a == b)
   {
-   printf("It is an Armstrong number");
+   printf("\nIt is an Armstrong number");
   }
   else
-   printf("It is not an Armstrong number");
+   printf("\nIt is not an Armstrong number");
 }
 
 int main(void)
 {
  int n=0,a=0,b=0;
- input(&n);
+ inputnumber(&n);
  armstrong(n,&a,&b);
- output(a,b);
+ outputresult(a,b);
  return 0;
 }
