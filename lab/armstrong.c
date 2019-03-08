ba7574ea1bb776e4844bcefc3cc9ddae3afa96bd
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-08 18:53:54 +0530

    Update armstrong.c

--- lab/armstrong.c
+++ lab/armstrong.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 void inputnumber(int* n)
 {
@@ -6,14 +7,26 @@ void inputnumber(int* n)
   scanf("%d",n);
 }
 
-void armstrong(int n,int* a,int* b)
+int limit(int n)
+{
+ int i=0,m;
+ m=n;
+ while(m!=0)
+ {
+  m=m/10;
+  i++;
+ }
+ return i;
+}
+
+void armstrong(int n,int* a,int* b,int c)
 { 
  int y=0;
   *b=n;   
  while (n != 0)
     {
         y = n % 10;
-        *a = *a +(y*y*y) ;
+        *a +=(pow(y,c)) ;
         n=n/10;
     }
 }
@@ -30,9 +43,10 @@ void outputresult(int a,int b)
 
 int main(void)
 {
- int n=0,a=0,b=0;
+ int n=0,a=0,b=0,c=0;
  inputnumber(&n);
- armstrong(n,&a,&b);
+ c=limit(n);
+ armstrong(n,&a,&b,c);
  outputresult(a,b);
  return 0;
 }
