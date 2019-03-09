4a2244689f644609acc715d970d6c82613f5f06a
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-09 11:16:04 +0530

    Update position.c

--- lab/position.c
+++ lab/position.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 
-void getn(int* n)
+int getn()
 {
+ int n;
  printf("Enter the no of elements ");
- scanf("%d",n);
+ scanf("%d",&n);
+ return n;
 }
  
-void input(int n,int x[],int* e)
+void input(int n,int x[])
 {
  printf("Enter the elements ");
  for(int i=0;i<n;i++)
    scanf("%d",&x[i]);
+}
+
+int searchelement()
+{
+ int e;
  printf("Enter the search element ");
- scanf("%d",e);
+ scanf("%d",&e);
+ return e;
 }
 
 void posi(int n,int x[],int e,int* m)
@@ -32,11 +40,11 @@ void output(int e,int m)
 int main(void)
 {
  int n;
- getn(&n);
+ n=getn();
  int x[n],e,m;
- input(n,x,&e);
+ input(n,x);
+ e=searchelement();
  posi(n,x,e,&m);
  output(e,m);
  return 0;
 }
- 
