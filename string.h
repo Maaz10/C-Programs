6319bc627414cc6a6707b09dd66d62305a20e0e6
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-15 18:22:58 +0530

    Update string.h

--- string.h
+++ string.h
@@ -3,50 +3,66 @@
 
 struct string
 {
- int n;
  int rows;
  char s1[100];
  char s2[100];
 };
 
-void input1(struct string *s)
+int input1()
 {
+ int n;
  printf("Enter the repetitions ");
- scanf("%d",&s->n);
+ scanf("%d",&n);
+ return n;
 }
 
-void input2(struct string *s)
+void input2(struct string s[])
 {
  printf("Enter the number of rows");
- scanf("%d",&s->rows);
+ scanf("%d",&s[0].rows);
 }
 
-void input3(struct string *s)
+void input3(struct string s[])
 {
  printf("Enter the first string ");
- scanf("%s",s->s1);
+ scanf("%s",s[0].s1);
  printf("Enter the second string ");
- scanf("%s",s->s2);
+ scanf("%s",s[1].s1);
 }
 
-void output(struct string s)
+void string(struct string s[])
 {
- char s3[100]="";
- printf("%s\n%s\n",s.s1,s.s2);
- for(int i=0;i<s.rows-2;i++)
+ strcpy(s[0].s2,s[0].s1);
+ strcpy(s[1].s2,s[1].s1);
+ for(int i=2;i<=s[0].rows;i++)
  {
-  strcat(s.s1,s.s2);
-  strcat(s3,s.s1);
-  printf("%s\n",s.s1);
+  strcat(s[i-2].s1,s[i-1].s1);
+  strcpy(s[i].s2,s[i-2].s1);
+  strcpy(s[i].s1,s[i-2].s1);
+ }
+}
+
+void output(struct string *s)
+{
+ printf("The string is\n");
+ for (int i=0;i<s[0].rows;i++)
+ {
+  printf("%s\n",s[i].s2);
  }
 }
 
 int main()
 {
- struct string s;
- input1(&s);
- input2(&s);
- input3(&s);
- output(s);
+ int n,i=0;
+ struct string s[10];
+ n=input1(s);
+ while(i<n)
+ {
+  input2(s);
+  input3(s);
+  string(s);
+  output(s);
+  i++;
+ }
  return 0;
 } 
