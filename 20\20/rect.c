1ff0e633fe194feca1e76946f0ce60f1ad55532d
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-18 16:51:55 +0530

    Update rect.c

--- 20\20/rect.c
+++ 20\20/rect.c
@@ -3,7 +3,7 @@
 
 struct vertice
 {
- float x1,y1,x2,y2,x3,y3,l,b,a;
+ float x1,y1,x2,y2,x3,y3,a,b,c,length,breadth,area;
 };
 
 int  instance()
@@ -24,16 +24,61 @@ void input(struct vertice *c)
  scanf("%f %f",&c->x3,&c->y3);
 }
 
+void length_breadth(struct vertice *c)
+{
+ c->a=sqrt(pow((c->x2-c->x1),2)+pow((c->y2-c->y1),2));
+ c->b=sqrt(pow((c->x3-c->x1),2)+pow((c->y3-c->y1),2));
+ c->c=sqrt(pow((c->x3-c->x2),2)+pow((c->y3-c->y2),2));
+ if(c->a<c->b && c->a<c->c)
+ {
+  if(c->b<c->c)
+  {
+   c->length=c->a;
+   c->breadth=c->b;
+  }
+  else
+  {
+   c->length=c->a;
+   c->breadth=c->c;
+  }
+ }
+ else
+  if(c->b<c->a && c->b<c->c)
+  {
+   if(c->a<c->c)
+   {
+    c->length=c->a;
+    c->breadth=c->b;
+   }
+   else
+   {
+    c->length=c->c;
+    c->breadth=c->b;
+   }
+  }
+  else
+  {
+   if(c->a<c->b)
+   {
+    c->length=c->c;
+    c->breadth=c->a;
+   }
+   else
+   {
+    c->length=c->c;
+    c->breadth=c->b;
+   }
+  }
+}
+
 void area(struct vertice *c)
 {
- c->l=sqrt(pow((c->x2-c->x1),2)+pow((c->y2-c->y1),2));
- c->b=sqrt(pow((c->x3-c->x1),2)+pow((c->y3-c->y1),2));
- c->a=c->l*c->b;
+ c->area=c->length*c->breadth;
 }
 
 void output(struct vertice c)
 {
- printf("The area is %f",c.a);
+ printf("The area is %f",c.area);
 }
 
 int main()
@@ -44,7 +89,8 @@ int main()
  while(i<n)
  {
   input(&c);
-  area(&c);
+  length_breadth(&c);
+  area(&c); 
   output(c);
   i++;
  }
