4c52b107e5f83f7b7919a6d80162ce05e97fb9b2
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-03-19 17:30:11 +0530

    Update egyptian.c

--- 20\20/egyptian.c
+++ 20\20/egyptian.c
@@ -16,22 +16,24 @@ int input(struct fraction *f)
  return n;
 }
 
-void compute(int n,struct fraction* f,double a[100])
+void compute(int n,struct fraction* f,int a[100])
 {
+ double temp;
  for(int i=0;i<n;i++)
  {
-  a[i]=ceil(f[i].deno/f[i].num);
+  temp=ceil(f[i].deno/f[i].num);
+  a[i]=temp;
   f[i+1].num=((f[i].num*a[i])-f[i].deno);
   f[i+1].deno=(f[i].deno*a[i]);
  }
 }
 
-void output(int n,double a[100])
+void output(int n,int a[100])
 {
  printf("The egyptian fractian\n");
  for(int i=0;i<n;i++)
  {
-  printf("+1/%.0lf",a[i]);
+  printf("+1/%d",a[i]);
  }
 }
 
@@ -40,7 +42,7 @@ int main()
  struct fraction f[100];
  int n;
  n=input(f);
- double a[n];
+ int a[n];
  compute(n,f,a);
  output(n,a);
  return 0;
