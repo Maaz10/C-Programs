d850ce1263ecdfbe642527197d79b0b84b344c11
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-06-21 12:45:42 +0530

    Create qsort-2d.c

---
+++ qsort-2d.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int getn()
+{
+    int n;
+    printf("Enter number of strings ");
+    scanf("%d",&n);
+    return n;
+}
+
+void input(int n,char s[n][25])
+{
+    printf("Enter the string ");
+    for(int i=0;i<n;i++)
+    {   
+        scanf("%s",s[i]);
+    }
+}
+
+int compare(const void *p1,const void *p2)
+{
+    return strcmp( *( char* const* )p1 , *( char* const* )p2 );
+}
+
+int main()
+{
+    int n;
+    n=getn();
+    char s[n][25];
+    input(n,s);
+    qsort(&s[0][0],n,sizeof(char*),compare);
+    printf("The string after sorting is\n");
+    for(int i=0;i<n;i++)
+    {
+     printf("%s",s[i]);
+    }    
+    return 0;
+}
