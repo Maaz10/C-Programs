7f136affc3385151e51953e2d131486e9fb19383
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-06-24 13:05:09 +0530

    Update charptr.c

--- charptr.c
+++ charptr.c
@@ -4,48 +4,51 @@
 
 int getn()
 {
-  int n;
-  printf("Enter the number of strings");
-  scanf("%d",&n);
-  return n;
+ int n;
+ printf("Enter the number of strings");
+ scanf("%d",&n);
+ return n;
 }
 
-void input(int n.char *s[n])
+void input(int n,char *s[n])
 {
- // char a[10];
-  for(int i=0;i<n;i++)\
-  {
-    printf("Enter string");
-    //scanf("%s",a);
-    scanf("%s",s[i]);
-    s[i] = (char*)malloc(strlen(s[i])*sizeof(char));
-    //s[i]=malloc(strlen(a)+1);
-   // strcpy(s[i],a);
-  }
+ char a[10];
+ for(int i=0;i<n;i++)\
+ {
+   printf("Enter string");
+   scanf("%s",a);
+   //scanf("%s",s[i]);
+   //s[i] = malloc((strlen(s[i])/**sizeof(char)*/)+1);
+   s[i]=malloc(strlen(a)+1);
+   strcpy(s[i],a);
+ }
 }
 
-int compare(const* void* p1,const* void* p2) 
+int compare(const void* p1,const void* p2) 
 {
-  return strcmp(*(const char**)p1, *(const char**)p2);
+ return strcmp(*(const char**)p1, *(const char**)p2);
 }
 
 void output(int n, char *s[n])
 {
-  printf("After sorting\n");
-  for(int i=0;i<n;i++)
-  {
-    printf("%s",s[i]);
-  }
+ printf("After sorting\n");
+ for(int i=0;i<n;i++)
+ {
+   printf("%s",s[i]);
+ }
 }
 
 void main()
 {
-  int n;
-  n=getn();
-  char *s[n];
-  input(n,s);
-  
-  qsort(&s[0][0],(n*10),sizeof(char),compare);
-  output(s);
+ int n;
+ n=getn();
+ char *s[n];
+ input(n,s);
+ 
+ qsort(s,n,sizeof(char*),compare);
+ output(n,s);
+for(int i=0;i<n;i++)
+{
+ free(s[i]);
 }
-  
+}
