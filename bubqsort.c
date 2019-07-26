0219fd35f29769a47e5e8d0b8f108e619f8dcc39
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-07-26 13:58:04 +0530

    Update bubqsort.c

--- bubqsort.c
+++ bubqsort.c
@@ -1,6 +1,6 @@
+
 #include<stdio.h>
 #include<stdlib.h>
-
 int getn()
 {
     int n;
@@ -18,37 +18,41 @@ void input(int n,int a[n])
     }
 }
 
-int compare(const void* a,const void* b)
-{
-     return (*(int*)a-*(int*)b);
-}
-
 void output(int n,int a[n])
 {
     printf("The array after sorting is ");
     for(int i=0;i<n;i++)
     {
-        printf(" %d",a[i]);
+        printf("%d ",a[i]);
     }
 }
- 
-void main()
+
+int compare(const void *a,const void *b)
 {
-    int n;
-    n=getn();
-    int a[n],x[2];
-    input(n,a);
+     return (*(int*)a-*(int*)b);
+}
+
+void myqsort(int n,int a[n],int size,int(*compare)(const void *p1,const void *p2))
+{
+    int x[2];
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<(n-i-1);j++)
         {
-            if(a[j]>a[j+1])
-            {
                 x[0]=a[j];
                 x[1]=a[j+1];
                 qsort(x,2,sizeof(int),compare);
-            }
         }
     }
+}
+
+void main()
+{
+    int n,size;
+    n=getn();
+    int a[n];
+    input(n,a);
+    size=sizeof(int);
+    myqsort(n,a,size,compare);
     output(n,a);
 }
