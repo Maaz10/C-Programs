1b48896fe85f69c5ac531b4aebaf0b2c726ded21
Maaz10 <46436454+Maaz10@users.noreply.github.com>
2019-11-11 02:48:15 +0530

    Update Dsa-proj-Toh-Stacks.c

--- lab-3/Dsa-proj-Toh-Stacks.c
+++ lab-3/Dsa-proj-Toh-Stacks.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>  
 #include <limits.h>  
 
-#define MAX 3
 
-void push(int *top,int stack[MAX],int val)
+void push(int n,int *top,int stack[n],int val)
 {
-    if(*top==MAX-1)
+    if(*top==n-1)
     {
         return ;
      }
@@ -14,7 +13,7 @@ void push(int *top,int stack[MAX],int val)
 	stack[*top]=val;
 }
 
-int pop(int *top,int stack[MAX])
+int pop(int n,int *top,int stack[n])
 {
     int z;
     if(*top==-1)
@@ -31,74 +30,79 @@ void moveDisk(char from, char to, int disk)
     printf("Move the disk %d from \'%c\' to \'%c\'\n",  disk, from, to);  
 } 
 
-void moveBetweenPoles(int *topx,int stackx[MAX],int *topy,int stacky[MAX], char s, char d)// src 1 dest 2 aux 3  
+void moveBetweenPoles(int n,int *topx,int stackx[n],int *topy,int stacky[n], char s, char d)// src 1 dest 2 aux 3  
 {  
-    int pole1TopDisk = pop(topx,stackx);  
-    int pole2TopDisk = pop(topy,stacky);  
+    int pole1TopDisk = pop(n,topx,stackx);  
+    int pole2TopDisk = pop(n,topy,stacky);  
   
     if (pole1TopDisk == -1)  
     {  
-        push(topx,stackx, pole2TopDisk);  
+        push(n,topx,stackx, pole2TopDisk);  
         moveDisk(d, s, pole2TopDisk);  
     }  
   
     else if (pole2TopDisk == -1)  
     {  
-        push(topy,stacky, pole1TopDisk);  
+        push(n,topy,stacky, pole1TopDisk);  
         moveDisk(s, d, pole1TopDisk);  
     }  
   
     else if (pole1TopDisk > pole2TopDisk)  
     {  
-        push(topx,stackx, pole1TopDisk);  
-        push(topx,stackx, pole2TopDisk);  
+        push(n,topx,stackx, pole1TopDisk);  
+        push(n,topx,stackx, pole2TopDisk);  
         moveDisk(d, s, pole2TopDisk);  
     }  
   
     else
     {  
-        push(topy,stacky, pole2TopDisk);  
-        push(topy,stacky, pole1TopDisk);  
+        push(n,topy,stacky, pole2TopDisk);  
+        push(n,topy,stacky, pole1TopDisk);  
         moveDisk(s, d, pole1TopDisk);  
     }  
 }
  
-void Toh(int num_of_disks,int top1,int stack1[MAX],int top2,int stack2[MAX],int top3,int stack3[MAX])  
+void Toh(int n,int top1,int stack1[n],int top2,int stack2[n],int top3,int stack3[n])  
 {  
     int i, total_num_of_moves,p=1;  
     char s = 'S', d = 'D', a = 'A';  
   
-    if (num_of_disks % 2 == 0)  
+    if (n % 2 == 0)  
     {  
         char temp = d;  
         d = a;  
         a = temp;  
     }  
-    for(int i=0;i<num_of_disks;i++)
+    for(int i=0;i<n;i++)
     {
      p=p*2;
     }
     total_num_of_moves = p - 1;  
   
-    for (i = num_of_disks; i >= 1; i--)  
-        push(&top1,stack1, i);  
+    for (i = n; i >= 1; i--)  
+        push(n,&top1,stack1, i);  
   
     for (i = 1; i <= total_num_of_moves; i++)  
     {  
         if (i % 3 == 1)  
-        moveBetweenPoles(&top1,stack1,&top2,stack2, s, d);  
+        moveBetweenPoles(n,&top1,stack1,&top2,stack2, s, d);  
   
         else if (i % 3 == 2)  
-        moveBetweenPoles(&top1,stack1,&top3,stack3, s, a);  
+        moveBetweenPoles(n,&top1,stack1,&top3,stack3, s, a);  
   
         else if (i % 3 == 0)  
-        moveBetweenPoles(&top3,stack3,&top2,stack2, a, d);  
+        moveBetweenPoles(n,&top3,stack3,&top2,stack2, a, d);  
     }  
 }  
 
 int main()  
 {  
-    int top1=-1,stack1[MAX],top2=-1,stack2[MAX],top3=-1,stack3[MAX];
-    Toh(MAX,top1,stack1,top2,stack2,top3,stack3);  
+    int n;
+    printf("\nEnter the number of disks: ");
+    scanf("%d",&n);
+    int top1=-1,stack1[n],top2=-1,stack2[n],top3=-1,stack3[n];
+    printf("\nThe Steps For Solving the Puzzle are :\n");
+    Toh(n,top1,stack1,top2,stack2,top3,stack3);  
+    printf("\n");
     return 0;  
 }  
